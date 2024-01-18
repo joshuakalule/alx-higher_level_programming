669262b73bff0e641770dbdeb11a2f41c3192a0c
Kalule Joshua <kaljoshkalule@gmail.com>
2024-01-18 21:51:44 +0000

    remove space

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,6 @@ CREATE TABLE IF NOT EXISTS second_table (
 	name VARCHAR(256),
 	score INT
 );
-
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'ALex', 3);
 INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
