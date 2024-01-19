64328b09338c05e4c32ae58fc23d3442821fb8fc
Kalule Joshua <kaljoshkalule@gmail.com>
2024-01-19 02:55:28 +0000

    add files 8 - 11

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,7 @@
+-- lists all the cities of California that can be found hbtn_0d_usa
+ SELECT id, name
+   FROM cities
+  WHERE cities.state_id = (
+		SELECT id
+		  FROM states
+		 WHERE name = 'Carlifornia');
