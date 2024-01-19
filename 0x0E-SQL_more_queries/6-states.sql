073ddc56e8102438651c9db5b8fd81ad87096ad9
Kalule Joshua <kaljoshkalule@gmail.com>
2024-01-19 03:06:18 +0000

    replace USER with USER

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa
 -- creates the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USER hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY UNIQUE NOT NULL
 	AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
