0ea2f2474fc8ce496b9370f1b47c93a37124d0ea
Kalule Joshua <kaljoshkalule@gmail.com>
2024-02-27 18:26:11 +0000

    sort cities in relationship by id

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -15,4 +15,4 @@ class State(Base):
     id = Column(Integer, unique=True, primary_key=True,
                 autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    cities = relationship("City", backref="state", order_by="City.id")
