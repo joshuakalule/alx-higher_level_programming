0dc2b813bb950339b6634c5c04644bbebb46235a
Kalule Joshua <kaljoshkalule@gmail.com>
2024-03-30 15:00:29 +0000

    correct output format

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,8 +6,10 @@ sends a request to the URL and displays the body.
 import requests
 import sys
 
-
 if __name__ == '__main__':
     url = sys.argv[1]
     r = requests.get(url)
-    print(r.status_code)
+    if r.status_code >= 400:
+        print(f"Error code: {r.status_code}")
+    else:
+        print(r.text)
